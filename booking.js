4a64e190c331f0973bba7cd72a0365b92ecc851e
Jose Villa <j0542420@amarillocollege.com>
2025-04-23 17:04:21 -0500

    Case Project 6

--- booking.js
+++ booking.js
@@ -16,3 +16,66 @@ Booking.prototype.getNights = function() {
     // returns the miliseconds into days
     return Math.round(diff / oneDay);
 }
+function initMap() {
+   
+    // Page objects
+    let displayMap = document.getElementById("displayMap");
+    let routeBox =    document.getElementById("routeBox");
+    
+    // Create a map to the Lakeside Haven
+    let LakesideHaven = {lat: 45.33809183897189, lng: -93.74729501849258};
+    
+    let myMap = new google.maps.Map(displayMap, {
+       zoom: 11,
+       center: LakesideHaven,
+       fullscreenControl: false
+    });
+    
+    // Add a marker for the Lakeside Haven
+    new google.maps.Marker({
+       position: {lat: 45.33809183897189, lng: -93.74729501849258},
+       map: myMap,
+       title: "Lakeside Haven"
+    });
+    
+    // Get the device's current position
+    navigator.geolocation.getCurrentPosition(getPos, handleError);
+    
+    function getPos(pos) {
+       let myPosition = {
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude
+       }
+             
+       console.log(myPosition);
+       
+       // Set up direction service and rendering
+       let routeFind = new google.maps.DirectionsService();
+       let routeDraw = new google.maps.DirectionsRenderer();
+          
+       // Drive from current location to Oak Top House
+       let  myRoute = {
+          origin: myPosition,
+          destination: LakesideHaven,
+          travelMode: "DRIVING"
+       }  
+ 
+       // Generate directions for the route
+       routeFind.route(myRoute, function(result, status) {
+          if (status == "OK") {
+             routeDraw.setDirections(result);
+             // Display route and directions
+             routeDraw.setMap(myMap);
+             routeDraw.setPanel(routeBox);
+          } else {
+             routeBox.textContent = "Directions Unavailable: " + status;
+          }
+       }); 
+       
+    } 
+    
+    // In case of geolocation error
+    function handleError(err) {
+       console.log("Geolocation error: " + err.message);
+    } 
+ }
